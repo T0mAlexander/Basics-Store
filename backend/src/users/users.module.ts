27af3d68d1061e075119d4a11b62619fb267ec7e
Tom Alexander <t0malexander@protonmail.com>
2023-05-22 18:25:34 -0300

    fix: :adhesive_bandage: fixed wrong modules assignment

--- backend/src/users/users.module.ts
+++ backend/src/users/users.module.ts
@@ -1,11 +1,11 @@
 import { Module } from '@nestjs/common'
 import { UsersService } from './users.service'
 import { UsersController } from './users.controller'
+import { PrismaService } from '../app.service'
 
 @Module({
-  imports: [],
   controllers: [UsersController],
-  providers: [UsersService]
+  providers: [PrismaService, UsersService]
 })
 
 export class UsersModule {}
