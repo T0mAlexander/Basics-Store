9f21bd55aacd593ba7ebe9b95ef04b021e41db3f
Tom Alexander <t0malexander@protonmail.com>
2023-05-22 18:29:11 -0300

    feat: :sparkles: created sellers modules file

---
+++ backend/src/sellers/sellers.module.ts
@@ -0,0 +1,10 @@
+import { Module } from '@nestjs/common'
+import { SellersService } from './sellers.service'
+import { SellersController } from './sellers.controller'
+import { PrismaService } from '../app.service'
+
+@Module({
+  controllers: [SellersController],
+  providers: [PrismaService, SellersService]
+})
+export class SellersModule {}
