7ee13d0843794cb186ead9757a001fa9a93d916d
Tom Alexander <t0malexander@protonmail.com>
2023-05-22 18:26:14 -0300

    chore: :truck: renamed prisma service

--- backend/src/users/users.service.ts
+++ backend/src/users/users.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@nestjs/common'
 import { Prisma, User } from '@prisma/client'
 import { hash } from 'bcryptjs'
 import { EmailExistsError } from './errors/EmailExists.error'
-import { PrismaService } from 'src/app.service'
+import { PrismaService } from '../app.service'
 
 @Injectable()
 export class UsersService {
-  constructor (readonly PrismaService: PrismaService) {}
+  constructor (private prisma: PrismaService) {}
 
   async CreateUser({ id, name, email, password, creation_date }: Prisma.UserCreateInput): Promise<User> {
     const password_secure = await hash(password, 6)
 
-    const EmailExists = await this.PrismaService.user.findUnique({
+    const EmailExists = await this.prisma.user.findUnique({
       where: { email }
     })
 
@@ -19,7 +19,7 @@ export class UsersService {
       throw new EmailExistsError()
     }
 
-    const User = await this.PrismaService.user.create({
+    const User = await this.prisma.user.create({
       data: {
         id,
         name,
