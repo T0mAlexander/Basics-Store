46c7e001679eff4be861f6d7ff83dede63070713
Tom Alexander <t0malexander@protonmail.com>
2023-05-23 01:28:30 -0300

    feat: :label: added DTO types
    The generated DTO file from Nest.js CLI was updated with types additions

--- backend/src/sellers/dto/create-seller.dto.ts
+++ backend/src/sellers/dto/create-seller.dto.ts
@@ -1,1 +1,16 @@
-export class CreateSellerDto {}
+import { IsEmail, IsNotEmpty, IsString, MinLength } from 'class-validator'
+
+export class CreateSellerDto {
+  seller_id: string
+
+  @IsString() @IsNotEmpty({ message: 'Name cannot be empty' })
+    name: string
+
+  @IsEmail(undefined, { message: 'Email is invalid!' })
+    email: string
+
+  @MinLength(6, { message: 'Password minimum length must be 6 characters!' })
+    password: string
+  
+  creation_date: string
+}
