38936707e641947ca043b05d43bcd037a2ea8471
Tom Alexander <t0malexander@protonmail.com>
2023-05-23 01:18:05 -0300

    build: :building_construction: updated sellers route services
    The sellers route services are now operational and currently being used by the seller route controller

--- backend/src/sellers/sellers.service.ts
+++ backend/src/sellers/sellers.service.ts
@@ -1,26 +1,52 @@
 import { Injectable } from '@nestjs/common'
-import { CreateSellerDto } from './dto/create-seller.dto'
-import { UpdateSellerDto } from './dto/update-seller.dto'
+import { Prisma, Seller } from '@prisma/client'
+import { hash } from 'bcryptjs'
+import { PrismaService } from '../app.service'
+import { EmailExistsError } from '../errors/EmailExists.error'
+import { SellerNotFoundError } from '../errors/SellerNotFound'
 
 @Injectable()
 export class SellersService {
-  create(createSellerDto: CreateSellerDto) {
-    return 'This action adds a new seller'
+  constructor (private prisma: PrismaService) {}
+
+  async CreateSeller({ seller_id, name, email, password, creation_date }: Prisma.SellerCreateInput): Promise<Seller> {
+    const password_secure = await hash(password, 6)
+
+    const EmailExists = await this.prisma.seller.findUnique({
+      where: { email }
+    })
+
+    if (EmailExists) {
+      throw new EmailExistsError()
+    }
+
+    const Seller = await this.prisma.seller.create({
+      data: {
+        seller_id,
+        name,
+        email,
+        password: password_secure,
+        creation_date
+      }
+    })
+
+    return Seller
   }
 
-  findAll() {
-    return `This action returns all sellers`
+  async DeleteUser(seller_id: string): Promise<Seller> {
+    const sellerIdExists = await this.prisma.seller.findUnique({
+      where: { seller_id }
+    })
+
+    if(!sellerIdExists) {
+      throw new SellerNotFoundError()
+    }
+
+    const SellerToDelete = await this.prisma.seller.delete({
+      where: { seller_id }
+    })
+
+    return SellerToDelete
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} seller`
-  }
-
-  update(id: number, updateSellerDto: UpdateSellerDto) {
-    return `This action updates a #${id} seller`
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} seller`
-  }
 }
