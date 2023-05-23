0299118a6b4d7ede334a55ca570176c5d8b98570
Tom Alexander <t0malexander@protonmail.com>
2023-05-23 01:09:26 -0300

    test: :white_check_mark: created E2E seller registration test
    A new endpoint test was created and was approved by Jest. The test was mirrored of E2E Users Registration since both have the same purpose

---
+++ backend/test/sellers.e2e-spec.ts
@@ -0,0 +1,41 @@
+import { INestApplication } from '@nestjs/common'
+import { Test } from '@nestjs/testing'
+import * as request from 'supertest'
+import { AppModule } from '../src/app.module'
+
+describe('Seller Registration (E2E)', () => {
+  let NestAppInstance: INestApplication
+  let sellerId: string
+
+  beforeAll(async () => {
+    const CoreModule = await Test.createTestingModule({
+      imports: [AppModule]
+    }).compile()
+
+    NestAppInstance = CoreModule.createNestApplication()
+    await NestAppInstance.init()
+  })
+
+  afterAll(async () => {
+    // Deleting the seller after the test
+    await request(NestAppInstance.getHttpServer())
+      .delete(`/sellers/${sellerId}`
+      ).expect(200)
+
+    // Ending the Nest.js server instance
+    await NestAppInstance.close()
+  })
+
+  test('Creating a new seller', async () => {
+    const response = await request(NestAppInstance.getHttpServer())
+      .post('/sellers')
+      .send({
+        name: 'Seller',
+        email: 'seller@email.com',
+        password: '123456'
+      })
+
+    expect(response.statusCode).toBe(201)
+    sellerId = response.body.seller_id
+  })
+})
