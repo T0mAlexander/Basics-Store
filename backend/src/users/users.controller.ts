a226e7827bb1ae437e64fcd6fcacd83e20480a04
Tom Alexander <t0malexander@protonmail.com>
2023-05-23 01:31:30 -0300

    feat: :triangular_flag_on_post: updated imports and added new route
    A new route to delete sellers has been added alongside imports fixes.

--- backend/src/users/users.controller.ts
+++ backend/src/users/users.controller.ts
@@ -1,9 +1,10 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
-import { Body, Controller, Post, Res } from '@nestjs/common'
+import { Body, Controller, Delete, Param, Post, Res } from '@nestjs/common'
 import { randomUUID } from 'crypto'
 import { Response } from 'express'
-import CreateUserDto from './dto/create-user.dto'
-import { EmailExistsError } from './errors/EmailExists.error'
+import { EmailExistsError } from '../errors/EmailExists.error'
+import { UserNotFoundError } from '../errors/UserNotFound'
+import { CreateUserDto } from './dto/create-user.dto'
 import { UsersService } from './users.service'
 
 @Controller('users')
@@ -36,5 +37,28 @@ export class UsersController {
 
       throw error
     }
+  }
+
+  @Delete(':id')
+  async DeleteUser(@Param('id') userId: string, @Res() Response: Response) {
+
+    try {
+      await this.UsersService.DeleteUser(userId)
+
+      return Response.status(200).json({
+        statusCode: Response.statusCode,
+        message: 'The user has been deleted from database!'
+      })
+    } catch (error) {
+      if (error instanceof UserNotFoundError) {
+        Response.status(404).json({
+          statusCode: Response.statusCode,
+          message: 'User not found in database!'
+        })
+      }
+
+      throw error
+    }
+
   }
 }
